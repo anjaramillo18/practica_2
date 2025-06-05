7436444ecf726124718d88395dd05f530a0aed48
anjaramillo18 <anahi2007jaramillo@gmail.com>
2025-06-04 21:14:51 -0500

    add: Proyecto inicial de practica 2 de programación.

---
+++ src/anjaramillo18/ejecutor/Practica_2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anjaramillo18.ejecutor;
+
+/**
+ *
+ * @author anahijaramillo
+ */
+public class Practica_2 {
+    public static void main(String[] args) {
+        
+    }
+}
